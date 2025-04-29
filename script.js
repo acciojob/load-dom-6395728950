2a89d8b809a35e5e36ee9a9ae3bac0b45492c3bb
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-04-29 17:42:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 //your JS code here. If required.
-window.addEventListener("load",()=>{
-	 const p = document.createElement("p");
-	p.textContent = "DOM load success";
-	p.style.color = "black";
-	document.body.appendChild(p);
+document.addEventListener("DOMContentLoaded",()=>{
+ document.body.innerHTML = "DOM load success";
 });
