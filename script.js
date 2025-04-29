7079316091647a0d46e0d697de0982fca88189ea
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-04-29 17:25:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+window.addEventListener("load",()=>{
+	 const p = document.createElement("p");
+	p.textContent = "DOM load success";
+	p.style.color = "black";
+	document.body.appendChild(p);
+});
